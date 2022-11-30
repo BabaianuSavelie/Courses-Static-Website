c6f874a21baefb5bd12f18396e6cd618293fd9b2
Babaianu Savelie <babaianu.savelie1@gmail.com>
2022-11-30 18:40:30 +0200

    Adding some responsivenes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-let typed = new Typed('.changeable', {
-    strings: ['JavaScript', 'React', 'Angular'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    loop: true,
-})
+let typed = new Typed(".changeable", {
+  strings: ["JavaScript", "React", "Angular"],
+  typeSpeed: 0,
+  backSpeed: 0,
+  loop: true,
+});
+
+const toggler = document.getElementById("toggler");
+const target = document.getElementById("target");
+toggler.addEventListener("click", () => {
+  target.classList.toggle("hidden");
+});
