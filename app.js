05a102669f2ef8ba1821329e87a99cf110bd71cc
Babaianu Savelie <babaianu.savelie1@gmail.com>
2022-12-05 19:19:55 +0200

    Some fixes

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ let typed = new Typed(".changeable", {
   loop: true,
 });
 
-const toggler = document.getElementById("toggler");
-const target = document.getElementById("target");
-toggler.addEventListener("click", () => {
+// const toggler = document.getElementById("toggler");
+// const target = document.getElementById("target");
+// toggler.addEventListener("click", () => {
+//   target.classList.toggle("hidden");
+// });
+
+const toggleBtn = document.querySelector("#toggler");
+const target = document.querySelector("#target");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("is-active");
   target.classList.toggle("hidden");
 });
